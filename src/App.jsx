8ab8f2d2fd727a8578dfbe9e84e7ec6872aec00e
negnq <117580177+neginashkan@users.noreply.github.com>
2023-04-01 13:50:26 +0430

    Set up intro section and add Google fonts for Quizzical App
    This commit sets up the intro section of the Quizzical App and adds all the Google font files. It also creates the HTML and CSS components for the intro section. The intro section will provide an introduction to the App and give users a brief overview of the features and capabilities.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState,useEffect } from 'react'
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
+// <Confetti width={width} height={height} />;
 import axios from "axios";
+
 // -------------------------------------------------------------
-import './CSS/styles.cc'
-    // <Confetti width={width} height={height} />;
+import Intro from "./Components/Intro"
+// -------------------------------------------------------------
+
+
+import './CSS/styles.css'
 function App() {
   return (
     <div className="App">
--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,5 +1,10 @@
 function Intro() {
-  return <div className="Intro"></div>;
+  return (
+    <div className="Intro">
+      <h1 className="title">Quizzical</h1>
+      <h2 className="subtitle">Let's get quizzical</h2>
+      <button className="settings-button">Settings</button>
+    </div>
+  );
 }
-
 export default Intro;
