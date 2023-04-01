b9af457d0c8a48fe60676e84b16e5f7947d4cfc4
negnq <117580177+neginashkan@users.noreply.github.com>
2023-04-01 16:47:00 +0430

    Finish styling the Score Message Box for Quizzical App
    This commit finishes styling the Score Message Box for the Quizzical App. It adds colors, fonts, and other styling elements to the Score Message Box. This will give the App a more polished and professional look, and make it easier for users to see their results.

--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -1,6 +1,13 @@
 function Answers() {
-  return <div className="Answers">
-    
-  </div>;
+  return (
+    <div className="Answers">
+      <p className="answer-massage">The quiz is finished!</p>
+      <button className="menu-button">Menu</button>
+      <p className="sub-answer-massage">
+        Correct answers: <span className="score-box">3 / 5</span>
+      </p>
+      <button className="play-again-button">Play again</button>
+    </div>
+  );
 }
 export default Answers;
